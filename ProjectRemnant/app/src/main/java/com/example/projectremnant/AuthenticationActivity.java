7edd337c970e2000b12cfbba9d492d45b03d03eb
Zakarie Ortiz <ZOrtiz42@outlook.com>
2019-09-11 21:15:02 -0400

    ADD - UI additions
    -Added Layout files for the Authentication screen and signup screen.
    
    -Added String resources for the following screens, Authentication and Signup.

---
+++ ProjectRemnant/app/src/main/java/com/example/projectremnant/AuthenticationActivity.java
@@ -0,0 +1,41 @@
+package com.example.projectremnant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authentication_activity);
+
+        Button btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: add method to authenticate the user.
+            }
+        });
+
+        Button btn_signup = findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: add method to launch the sign up activity.
+                signupTapped();
+            }
+        });
+    }
+
+    private void signupTapped() {
+        Intent i = new Intent(this, SignupActivity.class);
+        startActivity(i);
+    }
+
+
+}
